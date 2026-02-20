70f50079df5c82beeb3ba4578bdc0e3de3ffae25
Trijito Santoso <tsantoso@veracode.com>
2026-02-19 21:31:12 -0600

    added cd to project path

--- scripts/fix-sca-run.sh
+++ scripts/fix-sca-run.sh
@@ -5,25 +5,19 @@ export PATH=$PATH:~/veracode-cli-2/
 PROJECT_ROOT_DIR=''
 PROJECT_PATH="$GITHUB_WORKSPACE/source-code/$PROJECT_ROOT_DIR"
 
-# cd $PROJECT_PATH
-
 PARAMS=""
 if [ -n "${FIX_SCA_PARAMS}" ]; then
     echo "FIX_SCA_PARAM: $FIX_SCA_PARAMS"
     PARAMS="-i $FIX_SCA_PARAMS"
 fi
 
-echo "PARAMS: $PARAMS"
-
-echo "pwd: $(pwd)"
-echo "ls"
-ls -ltrh
-
 veracode fix sca "$PROJECT_PATH" \
     --results $GITHUB_WORKSPACE/$SCA_RESULTS_FILE_NAME \
     --transitive \
     --decouple true \
     $PARAMS
+
+cd $PROJECT_PATH
 
 # Check if there are any changes to existing files (excluding new files)
 if [ -z "$(git diff --name-only HEAD)" ]; then
