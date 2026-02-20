da5ea5673226ddf840e65f6b8ede852f59e631f9
Trijito Santoso <tsantoso@veracode.com>
2026-02-19 18:32:03 -0600

    test set up cli step

---
+++ scripts/fix-sca-setup-cli.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+mkdir -p ~/.veracode && touch ~/.veracode/veracode.yml
+
+cat > ~/.veracode/veracode.yml << EOF
+api:
+    key-id: $FIX_SCA_VERACODE_API_KEY_ID
+    key-secret: $FIX_SCA_VERACODE_API_KEY_SECRET
+oauth:
+    enabled: false
+    region: ""
+packager:
+    "":
+        trust: true
+    _users_someusers_project:
+        trust: true
+EOF
+
+# Extract Veracode CLI
+mkdir -p ~/veracode-cli-2
+unzip ./cli/veracode.zip  -d ~/veracode-cli-2
+chmod +x ~/veracode-cli-2/veracode
